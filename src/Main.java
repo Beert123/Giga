871a6c84af3b9e7adf1ee2f03a03a2b81dd40a1f
Beert123 <bertil@selmas.dk>
2024-02-08 10:52:47 +0100

    test hello world

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello, world!");
         int fuck = 0;
+
+        int fuck3 = 4;
         int fuck2 = 1;
     }
 }
