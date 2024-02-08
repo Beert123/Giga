0e78db5ac13c86ce8574068482ed602ca88fc8d8
Beert123 <bertil@selmas.dk>
2024-02-08 11:23:44 +0100

    Added parkeringsPlads.java klasse

--- src/Parkeringsplads.java
+++ src/Parkeringsplads.java
@@ -4,8 +4,16 @@ public class Parkeringsplads {
     private int nummer;
     private LocalTime ankomst;
 
+    private Bil bil;
+
     public Parkeringsplads(int nummer, LocalTime ankomst) {
         this.nummer = nummer;
         this.ankomst = ankomst;
+    }
+    public Bil getBil() {
+        return bil;
+    }
+    public void setBil(Bil bil) {
+        this.bil = bil;
     }
 }
