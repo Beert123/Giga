d9623051e401a99d091c490f6cf189211f51d8fc
Bjarke-Arnved <eaa23bjar@students.eaaa.dk>
2024-02-08 11:30:58 +0100

    added createParkeringsplads(int nummer) i Parkeringshus.

--- src/Parkeringshus.java
+++ src/Parkeringshus.java
@@ -1,8 +1,15 @@
+import java.util.ArrayList;
+
 public class Parkeringshus {
     private String addresse;
-
+    private ArrayList<Parkeringsplads> parkeringspladser;
     public Parkeringshus(String addresse) {
         this.addresse = addresse;
+        this.parkeringspladser = new ArrayList<Parkeringsplads>();
     }
+    public void createParkeringsplads(int nummer) {
+        Parkeringsplads parkeringsplads = new Parkeringsplads(nummer, null);
+        parkeringspladser.add(parkeringsplads);
+}
 
 }
