241c1be6d4286ee37511693e8b66e1168068ce8c
amel100k <eaa23ema@students.eaaa.dk>
2024-02-08 11:00:44 +0100

    added a fuck

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello, world!");
-        int fuck = 0;
+        testtest();
+        test2();
+        
+    }
 
-        int fuck3 = 4;
-        int fuck2 = 1;
+    private static void test2() {
+    }
+
+    private static void testtest() {
     }
 }
